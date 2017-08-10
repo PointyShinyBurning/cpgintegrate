d54544ecf6469eedce80d6d3180aa826c1fcc19a
PointyShinyBurning <PointyShinyBurning@gmail.com>
2017-08-10 12:47:34 +0100

    Add file source and subjectID to processing exceptions

--- cpgintegrate/__init__.py
+++ cpgintegrate/__init__.py
@@ -1,5 +1,4 @@
 import pandas
-import traceback
 import typing
 
 
@@ -7,10 +6,19 @@ def process_files(file_iterator: typing.Iterator[typing.IO], processor: typing.C
 
     def get_frames():
         for file in file_iterator:
-            df = processor(file)
+            source = getattr(file, 'name', None)
+            subject_id = getattr(file, 'cpgintegrate_subject_id', None)
+            try:
+                df = processor(file)
+            except Exception as e:
+                raise ProcessingException({"Source": source, 'SubjectID': subject_id}) from e
             yield (df
                    .assign(Source=getattr(file, 'name', None),
                            SubjectID=getattr(file, 'cpgintegrate_subject_id', None),
                            FileSubjectID=df.index if df.index.name else None))
 
     return pandas.DataFrame(pandas.concat((frame for frame in get_frames()))).set_index("SubjectID")
+
+
+class ProcessingException(Exception):
+    """cpgintegrate processing error"""
