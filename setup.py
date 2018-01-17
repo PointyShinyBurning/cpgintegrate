bb5b5b1e82d1f3e727e4185a9384b9b1b5c2118e
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-01-17 10:41:00 +0000

    Bump requests version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="cpgintegrate",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'requests>=2.10',
+        'requests>=2.18.4',
         'lxml>=3',
         'pandas>=0.18.1',
         'xlrd',
