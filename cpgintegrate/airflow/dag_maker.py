30d3e01c7ad0f6952c5b1d6c0ce0cc28d5696931
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-02-12 13:57:30 +0000

    Task_ids

--- cpgintegrate/airflow/dag_maker.py
+++ cpgintegrate/airflow/dag_maker.py
@@ -6,7 +6,8 @@ def dataset_list_to_ckan(target_dag: DAG, connector_class, connection_id,
                          ckan_connection_id, ckan_package_id, pool, dataset_list):
     with target_dag as dag:
         for dataset in dataset_list:
-            pull = CPGDatasetToXCom(connector_class=connector_class, connection_id=connection_id,
+            pull = CPGDatasetToXCom(task_id=dataset, connector_class=connector_class, connection_id=connection_id,
                                     dataset_args=[dataset], pool=pool)
-            push = XComDatasetToCkan(ckan_connection_id=ckan_connection_id, ckan_package_id=ckan_package_id)
+            push = XComDatasetToCkan(task_id=dataset+'_ckan_push',
+                                     ckan_connection_id=ckan_connection_id, ckan_package_id=ckan_package_id)
             pull >> push
