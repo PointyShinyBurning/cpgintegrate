78f841efa0d9b3f0a3c96aeaf408eb5b365a06f0
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-02-12 13:55:46 +0000

    Uses keyword arguments when initializing operators

--- cpgintegrate/airflow/dag_maker.py
+++ cpgintegrate/airflow/dag_maker.py
@@ -6,6 +6,7 @@ def dataset_list_to_ckan(target_dag: DAG, connector_class, connection_id,
                          ckan_connection_id, ckan_package_id, pool, dataset_list):
     with target_dag as dag:
         for dataset in dataset_list:
-            pull = CPGDatasetToXCom(connector_class, connection_id, dataset_args=[dataset], pool=pool)
-            push = XComDatasetToCkan(ckan_connection_id, ckan_package_id)
+            pull = CPGDatasetToXCom(connector_class=connector_class, connection_id=connection_id,
+                                    dataset_args=[dataset], pool=pool)
+            push = XComDatasetToCkan(ckan_connection_id=ckan_connection_id, ckan_package_id=ckan_package_id)
             pull >> push
