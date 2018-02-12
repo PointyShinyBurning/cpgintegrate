ff7016c29d911f05741cce119ec2bff139e628fe
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-02-12 13:34:56 +0000

    Arg order in dag_maker

--- cpgintegrate/airflow/dag_maker.py
+++ cpgintegrate/airflow/dag_maker.py
@@ -2,8 +2,8 @@ from airflow import DAG
 from airflow.operators.cpg_plugin import CPGDatasetToXCom, XComDatasetToCkan
 
 
-def dataset_list_to_ckan(target_dag: DAG, connector_class, connection_id, dataset_list,
-                         ckan_connection_id, ckan_package_id, pool):
+def dataset_list_to_ckan(target_dag: DAG, connector_class, connection_id,
+                         ckan_connection_id, ckan_package_id, pool, dataset_list):
     with target_dag as dag:
         for dataset in dataset_list:
             pull = CPGDatasetToXCom(connector_class, connection_id, dataset_args=[dataset], pool=pool)
