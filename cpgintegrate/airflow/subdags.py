8d3a5bb97d84e909262d07373b7a74f51b3ad1df
DanielKey <PointyShinyBurning@gmail.com>
2018-02-13 16:41:42 +0000

    Fix concurrency shenanigans

--- cpgintegrate/airflow/subdags.py
+++ cpgintegrate/airflow/subdags.py
@@ -7,9 +7,17 @@ def dataset_list_subdag(dag_id, start_date, connector_class, connection_id, ckan
     subdag = DAG(dag_id, start_date=start_date)
     with subdag as dag:
         for dataset in dataset_list:
+
             pull = CPGDatasetToXCom(task_id=dataset, connector_class=connector_class, connection_id=connection_id,
                                     dataset_args=[dataset], pool=pool)
             push = XComDatasetToCkan(task_id=dataset + '_ckan_push',
                                      ckan_connection_id=ckan_connection_id, ckan_package_id=ckan_package_id)
             pull >> push
+
+            # Puts them a row because airflow 1.9.0 doesn't respect pools in subDags
+            try:
+                last >> pull
+            except NameError:
+                pass
+            last = pull
     return subdag
