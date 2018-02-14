2c70065fa3cf80818128ba92cb9ef951d9b86f84
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-02-14 15:05:10 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="cpgintegrate",
-    version="0.2.14SNAPSHOT",
+    version="0.2.14",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
