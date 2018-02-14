439b2266f7199967055ba2165292d80f9eb6ffa3
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-02-14 12:47:05 +0000

    Corrects omron bp units

--- cpgintegrate/processors/omron_bp.py
+++ cpgintegrate/processors/omron_bp.py
@@ -64,7 +64,7 @@ def to_frame(file):
     sheet['Excessive Movements'] = len(data[data['Excessive Movement (y or n)'] == 'y'].index)
 
     new_col_names = [col.split('(')[0].strip() for col in sheet.columns]
-    col_info = {new: {cpgintegrate.UNITS_ATTRIBUTE_NAME: old.split('(')[1]}
+    col_info = {new: {cpgintegrate.UNITS_ATTRIBUTE_NAME: old.split('(')[1][:-1]}
                 for old, new in zip(sheet.columns, new_col_names) if '(' in old}
     sheet.columns = new_col_names
 
