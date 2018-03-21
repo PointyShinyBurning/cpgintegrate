ab4862b2069c97ce8fdcd541633ce55ca8e2099c
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-03-21 18:13:16 +0000

    Column order from ordering of FormRefs in xml file
    Bit hacky but seems to hold up

--- cpgintegrate/connectors/openclinica.py
+++ cpgintegrate/connectors/openclinica.py
@@ -38,8 +38,11 @@ class OpenClinica(FileDownloadingConnector):
             self.session.post(self.base_url + '/ChangeStudy',
                               data={'studyId': study_id, 'action': 'submit', 'Submit': 'Confirm'})
 
+    def _xpath(self, xpath):
+        return self.xml.xpath(xpath, namespaces=self.nsmap)
+
     def _get_xml(self):
-        if not self.xml:
+        if self.xml is None:
             if self.xml_path:
                 self.xml = etree.parse(self.xml_path).getroot()
             elif self.dataset_id:
@@ -55,15 +58,14 @@ class OpenClinica(FileDownloadingConnector):
                     % self.study_oid).content)
 
             self.nsmap = self.xml.nsmap
-            self.nsmap['default'] = self.nsmap.pop(None)
+            self.nsmap['d'] = self.nsmap.pop(None)
 
     def iter_files(self, item_oid: str) -> typing.Iterator[typing.IO]:
         self._get_xml()
-        items = self.xml. \
-            xpath(".//default:FormData[@OpenClinica:Status != 'invalid']"
-                  "/default:ItemGroupData"
-                  "/default:ItemData[@ItemOID = '%s']"
-                  % item_oid, namespaces=self.nsmap)
+        items = self._xpath(".//d:FormData[@OpenClinica:Status != 'invalid']"
+                            "/d:ItemGroupData"
+                            "/d:ItemData[@ItemOID = '%s']"
+                            % item_oid)
         for item in items:
             resp = self.session.get(self.base_url + "/DownloadAttachedFile", stream=True,
                                     params={'fileName': item.attrib['Value']})
@@ -71,14 +73,14 @@ class OpenClinica(FileDownloadingConnector):
             file_like.decode_content = True
             file_like.name = resp.url
             setattr(file_like, cpgintegrate.SUBJECT_ID_ATTR,
-                    item.xpath('ancestor::default:SubjectData', namespaces=self.nsmap)[0]
+                    item.xpath('ancestor::d:SubjectData', namespaces=self.nsmap)[0]
                     .get('{%s}StudySubjectID' % self.nsmap['OpenClinica']))
             yield file_like
 
     def list_datasets(self) -> set:
         if self.xml:
             return {'_'.join(elem.attrib['FormOID'].split('_')[:-1])
-                    for elem in self.xml.xpath('.//default:FormData', namespaces=self.nsmap)}
+                    for elem in self.xml.xpath('.//d:FormData', namespaces=self.nsmap)}
         else:
             return {'_'.join(elem['@OID'].split('_')[:-1]) for elem in
                     self.session.get(self.base_url + '/rest/metadata/json/view/%s/*/*' % self.study_oid)
@@ -90,8 +92,8 @@ class OpenClinica(FileDownloadingConnector):
         def item_col_name(item_data):
             return "_".join(
                 filter(None, [
-                    self.xml.xpath("./default:Study/default:MetaDataVersion/default:ItemDef[@OID='%s']"
-                                   % item_data.attrib['ItemOID'], namespaces=self.nsmap)[0].attrib['Name'],
+                    self._xpath("./d:Study/d:MetaDataVersion/d:ItemDef[@OID='%s']"
+                                % item_data.attrib['ItemOID'])[0].attrib['Name'],
                     item_data.getparent().attrib.get('ItemGroupRepeatKey')]))
 
         def form_to_dict(form):
@@ -99,7 +101,7 @@ class OpenClinica(FileDownloadingConnector):
                 return (
                     (item_col_name(item_data),
                      item_data.attrib.get('Value'))
-                    for item_data in item_group.xpath('./default:ItemData', namespaces=self.nsmap))
+                    for item_data in item_group.xpath('./d:ItemData', namespaces=self.nsmap))
 
             def attribute_dictize(xml_item):
                 return {
@@ -110,21 +112,19 @@ class OpenClinica(FileDownloadingConnector):
                 **attribute_dictize(form.find('../..')),
                 **attribute_dictize(form.find('..')),
                 **attribute_dictize(form),
-                **{k: v for item_group in form.xpath('./default:ItemGroupData', namespaces=self.nsmap)
+                **{k: v for item_group in form.xpath('./d:ItemGroupData', namespaces=self.nsmap)
                    for k, v in item_group_listize(item_group)})
 
         def get_item_info(item_oid):
             item_info = {
                 cpgintegrate.DESCRIPTION_ATTRIBUTE_NAME:
-                    self.xml.xpath(".//default:ItemDef[@OID='%s']" % item_oid,
-                                   namespaces=self.nsmap)[0].attrib.get('Comment')}
-            measurement_units = self.xml.xpath(".//default:ItemDef[@OID='%s']//default:MeasurementUnitRef"
+                    self._xpath(".//d:ItemDef[@OID='%s']" % item_oid)[0].attrib.get('Comment')}
+            measurement_units = self.xml.xpath(".//d:ItemDef[@OID='%s']//d:MeasurementUnitRef"
                                                % item_oid, namespaces=self.nsmap)
             if len(measurement_units):
                 item_info[cpgintegrate.UNITS_ATTRIBUTE_NAME] = \
-                    self.xml.xpath(".//default:MeasurementUnit[@OID='%s']"
-                                   % measurement_units[0].attrib.get('MeasurementUnitOID'),
-                                   namespaces=self.nsmap)[0].attrib.get('Name')
+                    self._xpath(".//d:MeasurementUnit[@OID='%s']"
+                                % measurement_units[0].attrib.get('MeasurementUnitOID'))[0].attrib.get('Name')
             return item_info
 
         def source_from_frame(frame):
@@ -137,23 +137,26 @@ class OpenClinica(FileDownloadingConnector):
 
         self._get_xml()
 
-        forms = self.xml.xpath(".//default:FormData[starts-with(@FormOID,'%s') and @OpenClinica:Status != 'invalid']"
-                               % form_oid_prefix, namespaces=self.nsmap)
+        forms = self._xpath(".//d:FormData[starts-with(@FormOID,'%s') and @OpenClinica:Status != 'invalid']"
+                            % form_oid_prefix)
 
         columns = set((item_data.attrib['ItemOID'], item_col_name(item_data)) for item_data
-                      in self.xml.xpath(".//default:FormData[starts-with(@FormOID,'%s') and"
-                                        " @OpenClinica:Status != 'invalid']//default:ItemData"
-                                        % form_oid_prefix, namespaces=self.nsmap))
+                      in self._xpath(".//d:FormData[starts-with(@FormOID,'%s') and"
+                                     " @OpenClinica:Status != 'invalid']//d:ItemData" % form_oid_prefix))
 
         column_info = {column_name: get_item_info(column_oid) for column_oid, column_name
                        in columns}
 
+        column_order = {column_name:
+                        self._xpath(".//d:ItemRef[@ItemOID='%s']" % column_oid)[0].sourceline
+                        for column_oid, column_name
+                        in columns}
+
         return (ColumnInfoFrame((form_to_dict(form) for form in forms), column_info=column_info)
                 .assign(**{cpgintegrate.SOURCE_FIELD_NAME: lambda frame: source_from_frame(frame)})
                 .set_index('SubjectData:StudySubjectID', drop=True)
-                .select(axis=1,
-                        crit=(lambda col: True) if include_meta_columns
-                        else (lambda col: (col in column_info.keys()
-                                           or col in ['FormData:Version', cpgintegrate.SOURCE_FIELD_NAME]))
-                        )
+                .loc[:, lambda f: sorted([col for col in f.columns if
+                                          (col in list(column_info.keys()) + [
+                                              'FormData:Version']) or include_meta_columns],
+                                         key=lambda col: column_order.get(col, 0))]
                 )
