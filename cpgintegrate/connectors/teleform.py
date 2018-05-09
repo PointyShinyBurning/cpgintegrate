8631fa28bc60037862ed6fc24c6783f5143954e7
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-05-09 12:57:56 +0100

    Teleform stuff

--- cpgintegrate/connectors/teleform.py
+++ cpgintegrate/connectors/teleform.py
@@ -10,11 +10,13 @@ class Teleform(Connector):
         super().__init__(**kwargs)
         self.dbString = 'postgresql://%s:%s@%s:%s/%s' % (auth[0], auth[1], host, port, schema)
 
-    def get_dataset(self, tbl, index_col='StudyID'):
+    def get_dataset(self, tbl, index_col='StudyID', index_col_is_subject_id=True):
         engine = sqlalchemy.create_engine(self.dbString)
 
         frame = pandas.read_sql_table(tbl, engine, parse_dates={}, coerce_float=False)
 
-        return (frame.set_index(index_col)
+        return (frame.pipe(lambda df: df.set_index(index_col) if index_col else df)
                 .assign(**{cpgintegrate.SOURCE_FIELD_NAME: engine.url.__to_string__(True)+'/'+tbl})
-                .rename_axis(cpgintegrate.SUBJECT_ID_FIELD_NAME))
+                .pipe(lambda df:
+                df.rename_axis(cpgintegrate.SUBJECT_ID_FIELD_NAME) if index_col and index_col_is_subject_id else df)
+                )
