602a79a1caa50e46f6eb5a97328e96bad6a3853f
PointyShinyBurning <PointyShinyBurning@gmail.com>
2018-08-22 14:22:31 +0100

    Adds interation over resources as well as dicoms for XNAT connector

--- cpgintegrate/connectors/xnat.py
+++ cpgintegrate/connectors/xnat.py
@@ -29,12 +29,17 @@ class XNAT(FileDownloadingConnector):
 
     def iter_files(self, experiment_selector=lambda x: True,
                    scan_selector=lambda x: True,
-                   image_selector=lambda x: True) -> typing.Iterator[typing.IO]:
+                   image_selector=lambda x: True,
+                   iter_resources=False) -> typing.Iterator[typing.IO]:
         experiments = self.get_dataset()
         for subject_id, experiment in experiments[experiments.apply(experiment_selector, axis=1)].iterrows():
-            scan_list = self._get_result_set(experiment.URI + "/scans")
+            scan_list =self._get_result_set(experiment.URI + "/resources") \
+                if iter_resources else self._get_result_set(experiment.URI + "/scans")
             for __, scan in scan_list[scan_list.apply(scan_selector, axis=1)].iterrows():
-                files = self._get_result_set(scan.URI+'/files')
+                if iter_resources:
+                    files = self._get_result_set(experiment.URI + '/resources/' + scan.xnat_abstractresource_id + '/files')
+                else:
+                    files = self._get_result_set(scan.URI+'/files')
                 for _, file_elem in files[files.apply(image_selector, axis=1)].iterrows():
                     file_url = self.base_url + file_elem.URI
                     file = self.session.get(file_url, auth=self.auth, stream=True).raw
