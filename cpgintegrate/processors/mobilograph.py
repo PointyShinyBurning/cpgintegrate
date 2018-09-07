b47936cdc07c3df19516b50b68b11dc9d93e3a95
DanielKey <PointyShinyBurning@gmail.com>
2018-09-07 13:39:40 +0100

    Stop mobilograph eating all the bytes

--- cpgintegrate/processors/mobilograph.py
+++ cpgintegrate/processors/mobilograph.py
@@ -1,4 +1,5 @@
 import pandas
+import io
 
 
 def to_frame(file):
@@ -7,10 +8,12 @@ def to_frame(file):
     :param file: A file-like object of the xls
     :return: A DataFrame containing results
     """
-    top_vars = (pandas.read_excel(file, nrows=8, header=None)
+    file_bytes = file.read()
+
+    top_vars = (pandas.read_excel(io.BytesIO(file_bytes), nrows=8, header=None)
                 .dropna(axis=1, how='all').set_index(0).T.assign(join_key=0)
                 )
 
-    measurements = pandas.read_excel(file, skiprows=13).assign(join_key=0).set_index('join_key')
+    measurements = pandas.read_excel(io.BytesIO(file_bytes), skiprows=13).assign(join_key=0).set_index('join_key')
 
     return top_vars.join(measurements, on='join_key').set_index('ID').drop('join_key', axis=1)
